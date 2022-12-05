4879c28fe3ede6dc4509c38212c068e4a292910e
Kellymugambi <kellymgambi@.com>
2022-12-05 03:01:43 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit",function(e){
+    e.preventDefault()
+    handletasks(e.target.new_task_description.value)
+    form.reset()
+  })
+  
 });
+function handletasks(new_task_description){
+ let p = document.createElement('p')
+ let btn = document.createElement('button')
+ btn.addEventListener("click", handledelete)
+ btn.textContent = 'x'
+ p.textContent = `${new_task_description} `
+ p.appendChild(btn)
+ console.log(p)
+ document.querySelector('#tasks').appendChild(p)
+
+}
+
+function handledelete(e){
+  e.target.parentNode.remove()
+}
